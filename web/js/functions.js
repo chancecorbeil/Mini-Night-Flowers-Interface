e54a040bc173b104fe3cc0511c3030cc7504f512
Chance Corbeil <corbeilchance@gmail.com>
2020-11-15 02:32:45 -0800

    Initial commit

---
+++ web/js/functions.js
@@ -0,0 +1,136 @@
+// Night Flowers v1.1.1
+// Functions
+
+// Begin wrap all functions - waits until document loads, then loads js
+$(document).ready(function(){
+
+// Load at website launch
+  $(".interface-1").load("list.html").stop().hide().fadeIn(1500); 
+
+// Interface sidebars
+  $("#sidebarnavigation").load("sidebarnavigation-1.html"); 
+  $("#sidebarnavigation2").load("sidebarnavigation-2.html"); 
+  $("#sidebarnavigation3").load("sidebarnavigation-3.html"); 
+
+// End wrap all functions
+});
+
+
+
+
+
+
+
+  // Load Interfaces
+  function loadinterface1(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-1").load("list.html").stop().hide().fadeIn(1600); 
+  }   
+  function loadinterface2(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-2").load("example-interface-2.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface3(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-3").load("example-interface-3.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface4(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-4").load("example-interface-4.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface5(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-5").load("example-interface-5.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface6(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-6").load("example-interface-6.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface7(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-7").load("example-interface-7.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface8(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-8").load("example-interface-8.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface9(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-9").load("example-interface-9.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface10(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-10").load("example-interface-10.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface11(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-11").load("example-interface-11.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface12(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-12").load("example-interface-12.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface13(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-13").load("example-interface-13.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface14(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-14").load("example-interface-14.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface15(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-15").load("example-interface-15.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface16(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-16").load("example-interface-16.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface17(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-17").load("example-interface-17.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface18(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-18").load("example-interface-18.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
+  function loadinterface19(){ 
+    $(".interface").stop().fadeOut(500); 
+    $(".interface-19").load("example-interface-19.html").stop().hide().fadeIn(1600); 
+    $.getScript("/js/functions-load-images.js", function() {});
+    $.getScript("/js/animations.js", function() {});
+  } 
